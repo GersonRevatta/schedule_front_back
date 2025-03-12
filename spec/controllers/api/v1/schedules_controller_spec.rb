27d5fff8fa26f0dff93d7890ece5053dc5e40c1e
gerson <gersonrevatta@gmail.com>
2025-03-12 18:00:46 -0500

    feat: fix specs on controller

--- spec/controllers/api/v1/schedules_controller_spec.rb
+++ spec/controllers/api/v1/schedules_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 require 'pry'
 
-
 RSpec.describe Api::V1::SchedulesController, type: :controller do
   describe 'POST #create' do
     let(:valid_params) do
@@ -20,32 +19,32 @@ RSpec.describe Api::V1::SchedulesController, type: :controller do
       {
         schedule: {
           startDate: Time.current + 2.days,
-          endDate: Time.current + 1.day # startDate mayor a endDate
+          endDate: Time.current + 1.day
         }
       }
     end
 
-    context 'cuando los parámetros son válidos' do
-      it 'crea un nuevo schedule' do
+    context 'params valids' do
+      it 'schedule success' do
         expect {
           post :create, params: valid_params, format: :json
         }.to change(Schedule, :count).by(1)
       end
 
-      it 'devuelve el schedule creado con estado HTTP 201' do
+      it 'schedule created' do
         post :create, params: valid_params, format: :json
         expect(response).to have_http_status(:created)
       end
     end
 
-    context 'cuando los parámetros son inválidos' do
-      it 'no crea un nuevo schedule' do
+    context 'params invalids' do
+      it 'schedule error' do
         expect {
           post :create, params: invalid_params, format: :json
         }.not_to change(Schedule, :count)
       end
 
-      it 'devuelve errores con estado HTTP 422' do
+      it 'schedule error' do
         post :create, params: invalid_params, format: :json
         expect(response).to have_http_status(:unprocessable_entity)
       end
